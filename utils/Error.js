954100ca2efb975873bc6fb56ba911185081572f
Skulldued <skulldued30@gmail.com>
2024-07-10 22:01:27 +0530

    Add a middleware and a custom Error handle function

---
+++ utils/Error.js
@@ -0,0 +1,7 @@
+//Creating Custom ERROR Here 
+export const errorHandler = (statusCode , message)=>{
+    const error = new Error();
+    error.message = message;
+    error.statusCode = statusCode;
+    return error;
+};
