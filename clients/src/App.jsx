0ea9544ed8e92a0561ad473338668aca88cd6f31
Skulldued <skulldued30@gmail.com>
2024-07-13 00:24:46 +0530

    update header component and make profile private

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import { BrowserRouter,Routes,Route} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home";
 import About from "../src/pages/About";
 import Signin from "../src/pages/Signin";
 import Profile from "../src/pages/Profile";
 import Signup from "../src/pages/Signup";
-import Header from './component/Header';
+import Header from "./component/Header";
+import PrivateRoute from "./component/PrivateRoute";
 const App = () => {
   return (
-    <BrowserRouter >
-    <Header/>
+    <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/sign-in" element={<Signin/>} />
-        <Route path="/sign-up" element={<Signup/>} />
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- clients/src/component/Header.jsx
+++ clients/src/component/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 const Header = () => {
+  const {currentUser} = useSelector(state =>state.user)
   return (
     <div className='bg-slate-200'>
       <div className='flex justify-evenly items-center py-2 px-20'>
@@ -10,7 +12,15 @@ const Header = () => {
         <ul className='flex gap-8'>
              <Link to="/"> <li>Home</li></Link>
              <Link to="/about"> <li>About</li></Link>
-             <Link to="/sign-in"> <li>Sign In</li></Link>
+             <Link to="/profile">
+              {
+                currentUser ? (
+                  <img src={currentUser.profilePicture} className='h-7 w-7 rounded-full object-cover' alt="" />
+                ):(
+                  <li>Sign In</li>
+                )
+              }
+              </Link>
         </ul>
       </div>
     </div>
