6138d95f731ea657297600d0e0426bb0452e6f5b
Skulldued <skulldued30@gmail.com>
2024-07-17 01:01:11 +0530

    Created about and home page

--- clients/src/pages/About.jsx
+++ clients/src/pages/About.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import about from "../assets/about.png";
 const About = () => {
   return (
-    <div className='bg-black h-screen'>
-     
+    <div className="bg-black w-full ">
+    <div className="max-w-[1200px] mx-auto px-6 ">
+      <div className="grid md:grid-cols-2">
+        <div className="py-10 md:py-32">
+          <img src={about} className="h-[450px]" alt="@human" />
+        </div>
+        <div className="py-10 md:py-32 flex items-center">
+          <div>
+            <h2 className="text-white text-3xl font-bold">
+            Passionate Full-Stack Developer in the Making
+            </h2>
+            <p className="py-8 text-slate-300 text-justify">
+            Welcome to my journey of becoming a full-stack developer with a focus on the MERN stack. My passion for web development drives me to continually learn and improve my skills, aiming to create dynamic and efficient web applications.
+            </p>
+           
+           
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
   )
 }
 
--- clients/src/pages/Home.jsx
+++ clients/src/pages/Home.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
-
+import React from "react";
+import home from "../assets/home.png";
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="bg-black w-full ">
+      <div className="max-w-[1200px] mx-auto px-6 ">
+        <div className="grid md:grid-cols-2">
+          <div className="py-10 md:py-32">
+            <img src={home} className="h-[450px]" alt="@human" />
+          </div>
+          <div className="py-10 md:py-32">
+            <div>
+              <h2 className="text-white text-3xl font-bold">
+                Welcome to My Profile 
+              </h2>
+              <p className="py-8 text-slate-300 text-justify">
+                Becoming a full-stack developer is a rewarding and challenging
+                journey that involves mastering both front-end and back-end
+                development skills. As a full-stack developer, I aim to create
+                seamless, dynamic, and responsive web applications by
+                understanding and implementing the complete spectrum of web
+                technologies.
+              </p>
+              <p className=" text-slate-300 text-justify">
+                On the front end, I focus on crafting intuitive and visually
+                appealing user interfaces using technologies like HTML, CSS, and
+                JavaScript. My recent experience with frameworks like ReactJS
+                and styling libraries such as TailwindCSS has enhanced my
+                ability to build responsive, component-based applications
+                efficiently. Leveraging tools like React Hook Form for form
+                validation ensures that the user experience is both smooth and
+                error-free.
+              </p>
+             
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
